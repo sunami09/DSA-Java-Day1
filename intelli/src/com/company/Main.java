23af51da297c3d4a4c546b4eb362fe6ae12b6a07
Sunami Dasgupta <sunamidasgupta@gmail.com>
2022-02-05 15:31:02 +0100

    Intelli

---
+++ intelli/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        Scanner rool = new Scanner(System.in);
+        System.out.println("Enter the roll Number");
+        int rool = input.nextInt();
+
+        System.out.println(rool);
+    }
+}
